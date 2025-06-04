8dd65aa8f5f0e12e69bc8928d1adff243d979872
krupasindhu-dev <krupasindhu.dev@gmail.com>
2025-06-04 19:17:22 +0530

    Refactor: Use LoginFormDynamic in Login page

--- fakecart/src/pages/Login.tsx
+++ fakecart/src/pages/Login.tsx
@@ -1,9 +1,11 @@
+import LoginFormDynamic from '../components/login/LoginFormDynamic';
 import LoginFormStatic from '../components/login/LoginFormStatic';
 
 const Login = () => {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-50">
-      <LoginFormStatic />
+      {/* <LoginFormStatic /> */}
+      <LoginFormDynamic />
     </main>
   );
 };
