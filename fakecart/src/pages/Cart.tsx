7da140c6dfa1734f8f793eb31f4313d10c653674
krupasindhu-dev <krupasindhu.dev@gmail.com>
2025-06-05 00:37:20 +0530

    Add CartDynamic component and commented CartStatic

--- fakecart/src/pages/Cart.tsx
+++ fakecart/src/pages/Cart.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import CartStatic from '../components/cart/CartStatic'
+import CartDynamic from '../components/cart/CartDynamic'
 
 
 
 const Cart = () => {
   return (
-    <div><CartStatic/></div>
+    <>
+        {/* <div><CartStatic/></div> */}
+        <div><CartDynamic/></div>
+    </>
+    
   )
 }
 
